29d00d73fd64c3395f8e1aa5e5b2d45516ef0edd
Philemon Eniola <dev.mcenny@gmail.com>
2022-11-12 23:54:39 +0100

    issues with connecting sequelize to mysql

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,97 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize()
+const sequelize = new Sequelize(
+  'realworlddb',
+  'realworlduser',
+  'realworldkey',
+ { 
+    host: 'localhost',
+    dialect: 'mysql' 
+}
+)
+
+const db = {}
+db.sequelize = sequelize;
+
+const Users = db.define('user', {
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    },
+    unique: true,
+    allowNull: false
+  },
+  username: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  bio: Sequelize.STRING,
+  image: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    validate: {
+      isUrl: true
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+const Articles = db.define('article', {
+  "slug": {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  "title": {
+    type: Sequelize.STRING(50),
+    allowNull: false
+  },
+  "description": {
+    type: Sequelize.STRING(100),
+  },
+  "body": Sequelize.STRING,
+})
+
+const Comments = db.define('comment', {
+  body: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+const Tags = db.define('tag', {
+  name: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  }
+})
+
+Comments.belongsTo(Articles)
+Articles.hasMany(Comments)
+
+Comments.belongsTo(Users, { as: 'author' })
+
+// // removed
+// Users.hasMany(Comments, {as: 'author'})
+
+Articles.belongsTo(Users, { as: 'author' })
+// Users.hasMany(Articles, {as: 'author'}) // modified
+Users.hasMany(Articles)
+
+Articles.belongsToMany(Users, { through: 'favourites' })
+Users.belongsToMany(Articles, { through: 'favourites' })
+
+Articles.belongsToMany(Tags, { through: 'article_tags' })
+Tags.belongsToMany(Articles, { through: 'article_tags' })
+
+
+module.exports = {
+  db,
+  Users, Articles, Comments, Tags
+}
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const db = require('./models')
+const morgan = require('morgan')
 
 const app = express();
 
 // For POST requests
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
 
 // Routes
 app.use('/api', require('./routes/api'))
 
-app.listen(2200, () => {
-    console.log('Server started on http://localhost:2200/')
-})
+db.sync({force: true})
+    .then(() => {
+        app.listen(2200, () => {
+            console.log('Server started on http://localhost:2200/')
+        })
+    })
+    .catch(() => {
+        console.log('an error db sync')
+    })
